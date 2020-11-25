f53187840f67b7362575f59a811a5714b57c2a3f
jackheuberger <jackheuberger5@gmail.com>
2020-11-24 19:59:24 -0600

    Added moment-timezone

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,15 +1,15 @@
-const moment = require('moment')
+const moment = require('moment-timezone')
 
 module.exports = {
-    formatDate: function(date, format) {
-        return moment(date).format(format)
+    formatDate: function (date, format) {
+        return moment(date).tz('America/New_York').tformat(format)
     },
-    stripTags: function(input) {
-        return input.replace(/<(?:.|\n)*?>/gm,'')
+    stripTags: function (input) {
+        return input.replace(/<(?:.|\n)*?>/gm, '')
     },
-    truncateBody: function(input) {
-        if(input.length > 100) {
-            return input.substring(0,100) + '...'
+    truncateBody: function (input) {
+        if (input.length > 100) {
+            return input.substring(0, 100) + '...'
         }
         return input
     }
