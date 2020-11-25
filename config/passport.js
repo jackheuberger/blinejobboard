282ec7b222d3028e554d0148a193d5bfe3fb2eb7
jackheuberger <jackheuberger5@gmail.com>
2020-11-24 19:54:07 -0600

    Formatting

--- config/passport.js
+++ config/passport.js
@@ -2,42 +2,43 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
 
-module.exports = function(passport) {
+module.exports = function (passport) {
     passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
-    },
-    async(accessToken, refreshToken, profile, done) => {
-        const newUser = {
-            googleId: profile.id,
-            displayName: profile.displayName,
-            email: profile.emails[0].value
-        }
-
-        if(newUser.email.includes('@psbma') || newUser.email.includes('@brooklinek12')){
-            newUser.student = true;
-            console.log(newUser)
-        }
-
-        try {
-            let user = await User.findOne({ googleId: profile.id })
-            
-            if(user){
-                done(null, user)
-            } else {
-                user = await User.create(newUser)
-                done(null, user)
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: '/auth/google/callback'
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                email: profile.emails[0].value
             }
-        }
-        catch(err){
-            console.error(err)
-        }
-    }))
 
-    passport.serializeUser((user,done) => done(null, user.id))
+            if (newUser.email.includes('@psbma') || newUser.email.includes('@brooklinek12')) {
+                newUser.student = true;
+                console.log(newUser)
+            }
 
-    passport.deserializeUser((id,done) => {
-        User.findById(id, (err, user) => done(err,user))
+            try {
+                let user = await User.findOne({
+                    googleId: profile.id
+                })
+
+                if (user) {
+                    done(null, user)
+                } else {
+                    user = await User.create(newUser)
+                    done(null, user)
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        }))
+
+    passport.serializeUser((user, done) => done(null, user.id))
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => done(err, user))
     })
 }
